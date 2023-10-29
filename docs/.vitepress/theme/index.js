4d639c84b9e07fdfc0faf37b4b3525254d3aeab7
吴习章 <wuxz14@vanke.com>
2023-10-29 10:17:15 +0800

    feat: vitepress 更新

--- docs/.vitepress/theme/index.js
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme'
-import MyLayout from './MyLayout.vue'
-import "../../public/reset.css";
-import "../../public/common.css";
-
-export default {
-  ...DefaultTheme,
-  Layout: MyLayout,
-};
